d2bea03881ddd49d5f475aa7623da18a783aa716
Emily Haynes <ehayne00@hotmail.com>
2019-11-19 15:16:18 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog 
+  def initialize(name) 
+    @name = name
+  end
+  def initialize(breed)
+    @breed = breed 
+  end
+  def name=(name)
+    @name = name
+  end
+   
+   def breed=(breed)
+     @breed = breed
+   end
+  
+end 
