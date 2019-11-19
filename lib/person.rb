c8dae2b88d4ddfa0388061898d55cb52099d321a
Emily Haynes <ehayne00@hotmail.com>
2019-11-19 15:44:22 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,6 +1,7 @@
 class Person
-def job=(job)
-  @job = job
+def job=(job) #argument name is same a local variable
+  @job = job#this is local variable  
+  #@job is instance variable; can be different from ocal variebale name/argument name.
 end
 def job
   @job
