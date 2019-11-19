0fb1a7b07cc126a53bb38514a28f9115285b3229
Emily Haynes <ehayne00@hotmail.com>
2019-11-19 15:35:31 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,18 @@
 class Dog 
-  def name=(name)
-    @this_dogs_name = name
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
   end
   
   def name
     @this_dogs_name
   end
    
-   def breed=(this_dogs_breed)
-     @this_dogs_breed = breed
+   def breed=(dogs_breed)
+     @this_dogs_breed = dogs_breed
    end
    
    def breed
-     @
+     @this_dogs_breed
+   end
   
 end 
